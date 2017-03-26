79bb75e149882341d1915feeb014a0046a76ba11
mark.hennessy <mark.hennessy@workday.com>
2017-03-26 20:59:23 +0100

    Documented Person class

--- src/flow_vs_typescript/flow/Person.js
+++ src/flow_vs_typescript/flow/Person.js
@@ -1,21 +1,46 @@
 // @flow
 /* eslint-disable no-console */
 
+/**
+ * A simple Person class created as an example of how to use flow types.
+ * @class
+ */
 class Person {
+  /**
+   * A public property that demonstrates a flow type annotation.
+   */
   name: string;
+
+  /**
+   * A constructor that demonstrates a flow param type annotations.
+   * @param name the person's name
+   */
   constructor(name: string) {
     this.name = name;
   }
 
+  /**
+   * A simple method that demonstrates a flow return type annotation.
+   * @returns {string} a greeting
+   */
   getGreeting(): string {
     return this.getSpecialGreeting();
   }
 
-// eslint-disable-next-line class-methods-use-this
+  /**
+   * An internal helper method
+   * @private
+   * @returns {string} a special greeting
+   */
+  // eslint-disable-next-line class-methods-use-this
   getSpecialGreeting(): string {
     return 'Welcome';
   }
 
+  /**
+   * A static method that logs 'Hi' to the console.
+   * It's best practice not to use the ': void' flow type annotation explicitly.
+   */
   static sayHi() {
     console.log('Hi');
   }
