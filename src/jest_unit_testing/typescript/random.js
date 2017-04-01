44fbad5800b697cc5873764572ffd1f709ca7658
mark.hennessy <mark.hennessy@workday.com>
2017-04-01 11:54:18 +0100

    Removed typescript output js files from repo and ignored them

--- src/jest_unit_testing/typescript/random.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sayHi = () => 'Hi';
-exports.addNumbers = (a, b) => a + b;
-// addNumbers('2', 5);
-//# sourceMappingURL=random.js.map
