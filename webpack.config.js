423670d506cb5737c62365334b691293a14b4cbc
mark.hennessy <mark.hennessy@workday.com>
2017-04-12 17:01:36 +0100

    Re-organized output directories

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 module.exports = {
   entry: ["babel-polyfill", "./src/codingame/skynet/flow/Player.js"],
   output: {
-    filename: "./bin/bundle.js"
+    filename: "./out/webpack/codingame/skynet/flow/Player.js"
   },
   module: {
     loaders: [{
