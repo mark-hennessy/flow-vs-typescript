3c52302a6c76a668bd3212a8afb25de6b7b17a69
mark.hennessy <mark.hennessy@workday.com>
2017-05-24 16:58:36 +0100

    Fixed js test config

--- beforeTest.js
+++ beforeTest.js
@@ -1,3 +1,4 @@
-// eslint-disable-next-line no-undef
-globalNumber = 7;
+/* eslint-disable no-new-func */
+const globalScope = Function('return this')();
+globalScope.globalNumber = 7;
 require('./src/jest-examples/typescript/initModel');
