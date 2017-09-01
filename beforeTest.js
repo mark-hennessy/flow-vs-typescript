2af108b5ea661762de775416e743583db79d7e21
mark.hennessy <mark.hennessy@workday.com>
2017-09-01 13:07:42 +0100

    Changed jestconfig back to json because IntelliJ runconfig didn't support js

--- beforeTest.js
+++ beforeTest.js
@@ -1,4 +1,5 @@
-/* eslint-disable no-new-func */
+// eslint-disable-next-line jsx-a11y/href-no-hash
+// eslint-disable-next-line no-new-func
 const globalScope = Function('return this')();
 globalScope.globalNumber = 7;
 require('./initModel');
