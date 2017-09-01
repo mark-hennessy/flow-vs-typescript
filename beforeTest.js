b46d7784d72a6ba713ec28d56264d6b5602a7b79
mark.hennessy <mark.hennessy@workday.com>
2017-09-01 14:20:52 +0100

    Commented config files

--- beforeTest.js
+++ beforeTest.js
@@ -1,5 +1,12 @@
 // eslint-disable-next-line jsx-a11y/href-no-hash
+
+// This strategy for accessing the global scope works for TS, JS, and Jest.
 // eslint-disable-next-line no-new-func
 const globalScope = Function('return this')();
 globalScope.globalNumber = 7;
+
+// Stub out log functions to reduce noise and make test results easier to read.
+console.log = () => {};
+
+// Initialize global namespaces etc.
 require('./initModel');
--- tslint.js
+++ tslint.js
@@ -1,6 +1,9 @@
+/* eslint-disable jsx-a11y/href-no-hash */
 module.exports = {
+  // https://www.npmjs.com/package/tslint-config-airbnb
   extends: 'tslint-config-airbnb',
   rules: {
+    // Configure when type definitions are required and when not.
     typedef: [
       true,
       'call-signature',
@@ -10,6 +13,7 @@ module.exports = {
       'object-destructuring',
       'array-destructuring',
     ],
+    // Enforce that there should be no space to the left of a colon and exactly 1 space after.
     'typedef-whitespace': [
       true,
       {
@@ -27,8 +31,20 @@ module.exports = {
         'variable-declaration': 'onespace',
       },
     ],
+    // Checks variable names for various errors.
+    'variable-name': [
+      true,
+      'ban-keywords',
+      'check-format',
+      'allow-pascal-case',
+    ],
+    // Requires the radix parameter to be specified when calling parseInt.
+    // This is disabled because a radix isn't always needed.
     radix: false,
+    // Applies a naming convention to function names and method names.
+    // This is disabled because it warns about signatures in type definition files.
     'function-name': false,
+    // Allow i++ and i--
     'no-increment-decrement': false,
   },
 };
